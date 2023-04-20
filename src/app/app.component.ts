d057acf60a4deb727ece2c82e493ca2200b01f49
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-20 09:47:11 -0300

    practicing directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task3-angular-directives';
+  isVisible = true;
+  buttonClicks: string[] = [];
+
+  setParagraphVisibility() {
+    this.isVisible = !this.isVisible;
+    this.buttonClicks.push('Clicked');
+  }
+
+  getBackgroundColor(index: number) {
+    return index >= 4 ? 'blue' : 'transparent';
+  }
+
+  getClass(index: number) {
+    return index >= 4;
+  }
 }
